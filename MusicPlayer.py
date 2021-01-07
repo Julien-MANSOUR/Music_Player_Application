ccb7c6c2bd0bf78b1df13c62da300341b6d6e610
Julien-MANSOUR <Julienmansour@gmail.com>
2021-01-07 21:54:57 +0100

    Creating and adding Layouts and groupbox for stylesheet

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -10,9 +10,28 @@ class Player(QWidget):
         self.show()
 
     def UI(self):
+        self.widgets()
+        self.layouts()
+    def widgets(self):
         pass
-
-
+    def layouts(self):
+        #################creating Layouts#####################
+        self.mainLayout=QVBoxLayout()
+        self.topMainLayout=QVBoxLayout()
+        #if we want to use stylesheet to our layouts we should use a groupe box first
+        self.topGroupBox=QGroupBox("Music Player",self)
+        self.topLayout=QHBoxLayout()
+        self.middleLayout=QHBoxLayout()
+        self.bottomLayout=QVBoxLayout()
+        ################Adding Widgets##########################
+        ###############Top Layout Widgets#######################
+        self.topMainLayout.addLayout(self.topLayout)
+        self.topMainLayout.addLayout(self.middleLayout)
+        self.topGroupBox.setLayout(self.topMainLayout)#so now the top and middle layout are now in a single groupe box forming the upper layout of main window
+        self.topGroupBox.setStyleSheet("background-color : orange")
+        self.mainLayout.addWidget(self.topGroupBox)#topgroupebox is a widget and not a layout
+        self.mainLayout.addLayout(self.bottomLayout)
+        self.setLayout(self.mainLayout)
 
 def main():
     App=QApplication(sys.argv)
